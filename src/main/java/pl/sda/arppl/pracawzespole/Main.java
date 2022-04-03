2540442d2266f7ca1f39bc6f27303a6604a344bb
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-04-03 20:12:09 +0200

    Zmiana_1

---
+++ src/main/java/pl/sda/arppl/pracawzespole/Main.java
@@ -0,0 +1,21 @@
+package pl.sda.arppl.pracawzespole;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj licznik");
+        int licznik = scanner.nextInt();
+
+        System.out.println("Podaj mianownik");
+        int mianownik = scanner.nextInt();{
+            if (licznik%mianownik == 0)
+                System.out.println("Liczba " + licznik + " Jest podzielna przez " + mianownik);
+            else
+                System.out.println("Liczba " + licznik + " Nie jest podzielna przez " + mianownik);
+        }
+
+    }
+}
